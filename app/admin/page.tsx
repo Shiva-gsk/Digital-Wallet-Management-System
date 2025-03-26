5b2d1d58b81186889f6e772c9f540f412016e761
Shiva <shivagulapala2005@gmail.com>
2025-03-27 00:06:24 +0530

    Added admin Page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,17 +1,34 @@
-
-import { redirect } from "next/navigation"
+"use client"
+import { useRouter } from 'next/navigation';
 import { useUser } from "@clerk/nextjs";
+import { useEffect, useTransition } from "react";
+import { fetchUserbyEmail } from "@/lib/getUser";
+import LoadingWidget from '@/components/LoadingWidget';
 export default function AdminDashboard() {
-  
-
-  const {user} = useUser();
+  const router = useRouter();
+  const { user } = useUser();
+  const [isPending, startTransition] = useTransition();
   // Check if user is not logged in or not an admin
-  
+  useEffect(() => {
+    startTransition(()=>{
+      if (user)
+        fetchUserbyEmail(user.emailAddresses[0]?.emailAddress).then((data) => {
+          if (data) {
+            if (data.role !== "ADMIN") {
+              router.push("/");
+            }
+          }
+        });
 
-  return (
-    <div>
+    })
+  }, [user]);
+
+  return (<>
+    {(isPending)? <LoadingWidget/> :<div>
       <h1>Admin Dashboard</h1>
       {/* Admin-only content */}
     </div>
-  )
+    }
+    </>
+  );
 }
